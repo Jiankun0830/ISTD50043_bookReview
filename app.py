56b66fd98dfe3433d8eae6ed65d5aa803393ad54
Emrys-Hong <hongpengfei.emrys@gmail.com>
2019-10-12 15:40:00 +0700

    Merge Yunyi's code with jiankun code

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import url_for,redirect,Flask,render_template
 import Service
 import mongoService
 
+
+
 app=Flask(__name__)
+
+
 
 @app.route("/")
 def home():
@@ -16,10 +20,6 @@ def about():
 def bookdemo():
     return url_for('about')
 
-@app.route("/t1")
-def hello1():
-    return "t1"
-
 @app.route("/sql")
 def des_sql():
     return (str(Service.Sql_db().describe()))
@@ -28,25 +28,43 @@ def des_sql():
 def book_list():
     return(str(mongoService.Mg().get_all()))
 
-@app.route("/bookindex/<page_num>")
+@app.route("/bookinfo/<page_num>")
 def book_list_page(page_num):
     page_num=int(page_num)
-    ls=mongoService.Mg().get_all()
-    total=len(ls)//20
+    book_list=mongoService.Mg().get_all_books()
+    total=len(book_list)//20
     if page_num==total:
-        return(str(ls[page_num*20:]))
+        temp_book_list = book_list[page_num*20:]
     elif page_num>total:
         return("no more books!")
     else:
-        return(str(ls[page_num*20:(page_num+1)*20])) 
+        temp_book_list = book_list[page_num*20:(page_num+1)*20]
+    return render_template("search.html", results=temp_book_list)
 
-@app.route("/bookinfo/<asin>")
-def info(asin):
-    book_info = mongoService.Mg().get_all_info(asin)
+@app.route("/book/<asin>")
+def show_book(asin):
+    book_info=mongoService.Mg().get_all_info(asin)
+    #return(render_template("info.html",book_info=book_info))
     return render_template("info.html", book_info=(5, 1002959, "JiankunTest", "Jiankun", 2019), reviews={"acc_id":[],"comment":[],"rating":[]}, rating=5)
+
+@app.route("/dashboard")
+def dashboard():
+    return(render_template("dashboard.html"))
+
+@app.route("/login")
+def login():
+    return(render_template("login.html"))
+
+@app.route("/registration")
+def registration():
+    return(render_template("registration.html"))
+
+@app.route("/search")
+def search():
+    return(render_template("search.html"))
+
 
 
 if __name__=="__main__":
-    app.debug = True
     app.run(debug=True)
 
