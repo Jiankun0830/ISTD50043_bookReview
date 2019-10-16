00b6de1079ea2724f1e68d04f02fac7f068ab5e1
Jiankun0830 <jiankun_lu@mymail.sutd.edu.sg>
2019-10-16 17:17:54 +0800

    Reformat code

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import url_for,redirect,Flask,render_template
+from flask import url_for, redirect, Flask, render_template
 from flask import request
-from flask import url_for,redirect,Flask,render_template,request
+from flask import url_for, redirect, Flask, render_template, request
 import SQLservice
 import mongoService
 import numpy as np
 import pandas as pd
 import json
-
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -16,6 +15,7 @@ mg = mongoService.Mg()
 with open('categories.json') as f:
     data = json.load(f)
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -23,16 +23,16 @@ def home():
 
 @app.route("/bookinfo")
 def book_list():
-    return(str(mg.get_all()))
+    return str(mg.get_all())
 
 
 @app.route("/bookinfo/<page_num>/<category>")
 def book_list_page(page_num, category):
-    page_num=int(page_num)
-    book_list=mongoService.Mg().get_all_books(page_num, category)
+    page_num = int(page_num)
+    book_list = mongoService.Mg().get_all_books(page_num, category)
     page_numbers = list(range(1, 4000))
     categories = ['Books', 'Behavioral Sciences', 'Relationships']
-    mg.insert_query({'results':book_list, 'page_numbers':page_numbers, 'categories':data})
+    mg.insert_query({'results': book_list, 'page_numbers': page_numbers, 'categories': data})
     return render_template("booklist.html", results=book_list, page_numbers=page_numbers, categories=data)
 
 
@@ -46,28 +46,28 @@ def info(asin):
     book_info = mongoService.Mg().get_all_info(asin)[0]
     results = SQLservice.SQL_db().get_review(asin)
     rating = np.mean([review[2] for review in results])
-    mg.insert_query({'book_info':book_info, 'reviews': results, 'rating': rating})
+    mg.insert_query({'book_info': book_info, 'reviews': results, 'rating': rating})
     return render_template("info.html", book_info=book_info, reviews=results, rating=rating)
 
 
 @app.route("/dashboard")
 def dashboard():
-    return (render_template("dashboard.html"))
+    return render_template("dashboard.html")
 
 
 @app.route("/login")
 def login():
-    return (render_template("login.html"))
+    return render_template("login.html")
 
 
 @app.route("/registration")
 def registration():
-    return (render_template("registration.html"))
+    return render_template("registration.html")
 
 
 @app.route("/search")
 def search():
-    return (render_template("booklist.html"))
+    return render_template("booklist.html")
 
 
 @app.route("/addbook", methods=['POST', 'GET'])
@@ -84,11 +84,12 @@ def addBook():
             buyAfterViewing = request.form['field8'].strip().split(" ")
             boughtTogether = request.form['field9'].strip().split(" ")
             category = [request.form['field10'].strip().split(" ")]
-            mg.add_book(asin, title=title, price=price, imUrl=url, category=category, brand=brand, also_bought=alsoBought, also_viewed=alsoViewed, buy_after_viewing=buyAfterViewing, bought_together=boughtTogether)
-            return(render_template("addbook.html"))
+            mg.add_book(asin, title=title, price=price, imUrl=url, category=category, brand=brand,
+                        also_bought=alsoBought, also_viewed=alsoViewed, buy_after_viewing=buyAfterViewing,
+                        bought_together=boughtTogether)
+            return render_template("addbook.html")
     else:
-        return(render_template("addbook.html"))
-
+        return render_template("addbook.html")
 
 
 if __name__ == "__main__":
--- mongoService.py
+++ mongoService.py
@@ -1,75 +1,76 @@
 from pymongo import MongoClient
 import time
+
+
 class Mg:
     def __init__(self):
-        self.con=MongoClient("mongodb://localhost:27017/")["book_metadata"]["metadata"]
+        self.con = MongoClient("mongodb://localhost:27017/")["book_metadata"]["metadata"]
         self.log = MongoClient("mongodb://localhost:27017/")["book_log"]["log"]
-  
-    def get_all_info(self,param):
-        a=self.con.find({"asin":param})
-        ls=[]
+
+    def get_all_info(self, param):
+        a = self.con.find({"asin": param})
+        ls = []
         for i in a:
             ls.append(i)
         return ls
-    
-    def add_book(self, asin, title=None, price=None, imUrl=None, category=[], salesRank={}, brand=None, also_bought=[], also_viewed=[], buy_after_viewing=[], bought_together=[]):
+
+    def add_book(self, asin, title=None, price=None, imUrl=None, category=[], salesRank={}, brand=None, also_bought=[],
+                 also_viewed=[], buy_after_viewing=[], bought_together=[]):
         # asin, title, brand, imUrl: string
         # salesRank={category: integer}
         # category: list of list
-        cursor = self.con.find({'asin':asin})
+        cursor = self.con.find({'asin': asin})
         if cursor.count() > 0:
             raise Exception('Book with given asin already exists.')
         else:
-            toInsert = {'asin':asin,
-                    'title': title,
-                    'price': price,
-                    'imUrl': imUrl,
-                    'related':{'also_bought':also_bought,
-                        'also_viewed':also_viewed,
-                        'buy_after_viewing':buy_after_viewing,
-                        'bought_together':bought_together},
-                    'categories':category,
-                    'salesRank':salesRank,
-                    'brand':brand}
+            toInsert = {'asin': asin,
+                        'title': title,
+                        'price': price,
+                        'imUrl': imUrl,
+                        'related': {'also_bought': also_bought,
+                                    'also_viewed': also_viewed,
+                                    'buy_after_viewing': buy_after_viewing,
+                                    'bought_together': bought_together},
+                        'categories': category,
+                        'salesRank': salesRank,
+                        'brand': brand}
             x = self.con.insert_one(toInsert)
+
     def get_total(self):
-        a=self.con.find().count()
+        a = self.con.find().count()
         # print(a)
-    
+
     def get_all_books(self, skip, category):
-        if(category=="all"):
+        if (category == "all"):
             param = {}
         else:
-            param = {'categories':{'$elemMatch':{'$elemMatch':{'$in':[category]}}}}
-        # print(param)
-        a=self.con.find(param).limit(100).skip(100*(skip-1))
-        # print(a)
-        ls=[]
+            param = {'categories': {'$elemMatch': {'$elemMatch': {'$in': [category]}}}}
+        a = self.con.find(param).limit(100).skip(100 * (skip - 1))
+        ls = []
         for i in a:
             ls.append(i)
-            
+
         return ls
 
     def get_all(self):
-        a=self.con.distinct("asin")
-        ls=[]
+        a = self.con.distinct("asin")
+        ls = []
         for i in a:
             ls.append(i)
         return ls
-    
-    def get_category(self,param):
-        p = {'categories':{'$elemMatch':{'$elemMatch':{'$in':[param]}}}}
+
+    def get_category(self, param):
+        p = {'categories': {'$elemMatch': {'$elemMatch': {'$in': [param]}}}}
         # print(p)
-        a=self.con.find(p)
+        a = self.con.find(p)
         # print(a)
-        ls=[]
+        ls = []
         for i in a:
             ls.append(i)
         return ls
-    
+
     def get_sorted_title(self):
         pass
-
 
     def insert_query(self, query):
         toInsert = {
@@ -77,4 +78,3 @@ class Mg:
             'timestamp': time.time()
         }
         x = self.log.insert_one(toInsert)
-
