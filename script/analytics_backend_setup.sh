4a890ecad516260cb2912d09154e95ab314307c3
Peng Shanshan <37920922+PengShanshan99@users.noreply.github.com>
2019-12-22 22:37:34 +0800

    Create analytics_backend_setup.sh

---
+++ script/analytics_backend_setup.sh
@@ -0,0 +1,24 @@
+python3 -m pip3 install flintrock
+
+read -p "Enter number of slaves: " NUM
+# read -p "Enter instance type: " TYPE
+# read -p "Enter key name: " KEY
+# read -p "Enter key path: " KEY_PATH
+
+flintrock launch good_shelf_grp7 \
+    --num-slaves $NUM \
+    --spark-version 2.4.4 \
+    --hdfs-version 2.7.7 \
+    --ec2-key-name group7-bigdata-ec2-key \
+    --ec2-identity-file group7-bigdata-ec2-key.pem \
+    --ec2-ami ami-000b133338f7f4255 \
+    --ec2-user ec2-user \
+    --ec2-instance-type t2.small \
+    --ec2-region us-west-2 \
+    --install-hdfs \
+    --install-spark
+
+python3 setup_masternode.py good_shelf_grp7
+
+#flintrock login databass_cluster
+# ami-06d51e91cea0dac8d 
